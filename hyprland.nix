7dda23ee1d9e56b59e680394c47b0da66cf6bbff
Willsen S. Wijaya <willsensw@proton.me>
2026-04-22 11:36:28 +0700

    remove old config

--- hyprland.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-	programs.hyprland = {
-    		enable = true;
-    		withUWSM = true; # recommended for most users
-    		xwayland.enable = true; # Xwayland can be disabled.
-  	};	
-}
