b6d1f74f655f5af80525ec4c8d28f87e3db6cbc9
Willsen S. Wijaya <willsensw@proton.me>
2026-05-14 20:21:19 +0700

    update to gnome

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+{
+    services.xserver = {
+        displayManager.gdm.enable = true;
+        desktopManager.gnome.enable = true;
+    };
+
+    environment.gnome.excludePackages = (with pkgs; [
+        gnome-photos
+        gnome-tour
+        cheese
+        epiphany
+        geary
+        evince
+        totem
+        tali
+        iagno
+        hitori
+        atomix
+    ]);
+
+    environment.systemPackages = with pkgs; [
+        gnome-tweaks
+        gnomeExtensions.user-themes
+        gnomeExtensions.appindicator
+        gnomeExtensions.dash-to-dock
+    ];
+
+    environment.sessionVariables = {
+        NIXOS_OZONE_WL = "1";
+    };
+}
