d4adf78ad088ce1f82c1e02ff0c377e621cbff8b
Willsen S. Wijaya <willsensw@proton.me>
2026-06-07 06:10:54 +0700

    Update game.nix

--- modules/software/game.nix
+++ modules/software/game.nix
@@ -14,6 +14,13 @@
     protonup-qt
     heroic
     lutris
-    pvzge
+
+    (pvzge.overrideAttrs (oldAttrs: rec {
+      version = "0.9.0";
+      src = fetchurl {
+        url = "https://github.com/Gzh0821/pvzg_site/releases/download/0.9.0/pvzge-0.9.0.zip"; 
+        sha256 = "d19e2fba24c650cdf581809add17934c5483f31d47ec20d13b78c725e4905087"; 
+      };
+    }))
   ];
 }
