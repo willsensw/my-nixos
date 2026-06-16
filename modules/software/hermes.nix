bb3d1fe2a7e54c748e7e305427865950de638b71
Willsen S. Wijaya <willsensw@proton.me>
2026-06-16 07:20:01 +0700

    Update hermes.nix

--- modules/software/hermes.nix
+++ modules/software/hermes.nix
@@ -4,11 +4,8 @@
     services.hermes-agent = {
         enable = true;
         settings.model.default = "anthropic/claude-sonnet-4";
-        environmentFiles = [ config.sops.secrets."hermes-env".path ];
+        environmentFiles = [ "/etc/hermes/api.env" ];
         addToSystemPackages = true;
-
-        container.enable = true;
-        container.hostUsers = [ "beimao" ];
     };
 }
 
