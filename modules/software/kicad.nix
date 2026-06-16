dddf86e15ce0861dc4a3a25f6220d1747b8f3160
Willsen S. Wijaya <willsensw@proton.me>
2026-06-16 07:54:15 +0700

    Update kicad.nix

--- modules/software/kicad.nix
+++ modules/software/kicad.nix
@@ -2,12 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [
-    (kicad.override {
-      addons = with pkgs.kicadAddons; [
-        kikit
+    # (kicad.override {
+      # addons = with pkgs.kicadAddons; [
+        # kikit
         # freerouting
-      ];
-    })
+      # ];
+    # })
     gerbv # Excellent gerber viewer to double-check your files before ordering
   ];
 }
