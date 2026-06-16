a8918e2e29b8b8a1df6cb8cced6bd98f72e07bac
Willsen S. Wijaya <willsensw@proton.me>
2026-06-16 09:10:10 +0700

    Update hermes.nix

--- modules/software/hermes.nix
+++ modules/software/hermes.nix
@@ -6,10 +6,22 @@
         container = {
             enable = true;
             hostUsers = [ "your-username" ];
+            backend = "podman";
         };
+
+        virtualisation.docker.enable = false;
+
         settings.model.default = "anthropic/claude-sonnet-4";
         environmentFiles = [ "/etc/hermes/api.env" ];
         addToSystemPackages = true;
     };
+
+    security.sudo.extraRules = [{
+        users = [ "willsen" ];
+        commands = [{
+          command = "/run/current-system/sw/bin/podman";
+          options = [ "NOPASSWD" ];
+        }];
+    }];
 }
 
