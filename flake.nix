683f4166d43c2e5d0dc3b1495f1b0825cc861fd3
Willsen S. Wijaya <willsensw@proton.me>
2026-08-05 07:25:54 +0700

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v1.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hermes-agent = {
+	  url = "github:NousResearch/hermes-agent";
+	};
+  };
+
+  outputs = { self, nixpkgs, hermes-agent, lanzaboote, home-manager, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      
+      # Pass inputs to your configuration.nix
+      specialArgs = { inherit inputs; };
+
+      modules = [
+        lanzaboote.nixosModules.lanzaboote
+		hermes-agent.nixosModules.default
+        ./configuration.nix
+
+	      home-manager.nixosModules.home-manager
+	      {
+	        home-manager = {
+	        useGlobalPkgs = true;
+	        useUserPackages = true;
+	        users.beimao = import ./home.nix;
+	        backupFileExtension = "backup";
+	        };
+	      }
+      ];
+    };
+  };
+}
