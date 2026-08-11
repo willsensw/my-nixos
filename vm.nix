fd9cb0fb7554740bf3ae29891c3c8c6a6ed65818
Willsen S. Wijaya <willsensw@proton.me>
2026-08-12 01:08:38 +0700

    Update vm.nix

--- vm.nix
+++ vm.nix
@@ -1,3 +1,17 @@
+{ config, ... }
 
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
+{
+  virtualisation = {
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
+  };
+
+  users.groups.libvirtd.members = ["beimao"];
+  
+  programs.virt-manager.enable = true;
+  
+  services = {
+    qemuGuest.enable = true;
+    spice-vdagentd.enable = true;
+  };
+}
