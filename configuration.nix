fce9a0651c89e3e8de0eeab31e43b974992ac7ba
Willsen S. Wijaya <willsensw@proton.me>
2026-08-14 19:30:38 +0700

    Update

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
       ./modules/desktop/boot.nix
       ./modules/desktop/gnome.nix
       ./modules/desktop/zsh.nix
+      ./modules/desktop/noctalia.nix
       ./modules/desktop/niri.nix
 
       # Hardware
---
+++ modules/desktop/noctalia.nix
@@ -0,0 +1,7 @@
+{ inputs, pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
+}
