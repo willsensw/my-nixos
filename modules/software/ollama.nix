4c5446c1562fe9da667f59664cb8aa9ed84271b5
Willsen S. Wijaya <willsensw@proton.me>
2026-08-15 23:36:29 +0700

    Update ollama.nix

--- modules/software/ollama.nix
+++ modules/software/ollama.nix
@@ -5,7 +5,11 @@
   		enable = true;
 		package = pkgs.ollama-cuda;
 	};
-  	
+
+	services.open-webui = {
+    	enable = true;
+  	};
+
 	# This prevents the service from starting automatically on boot
   	systemd.services.ollama.wantedBy = lib.mkForce [ ];
 }
