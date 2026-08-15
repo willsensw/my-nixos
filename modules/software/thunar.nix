68da07db358796a0046b92c4d8c1eba130fd8a7e
Willsen S. Wijaya <willsensw@proton.me>
2026-08-15 15:41:35 +0700

    Configuring Thunar

--- modules/software/thunar.nix
+++ modules/software/thunar.nix
@@ -1,7 +1,20 @@
 { config, pkgs, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
-        thunar
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-volman
+      thunar-archive-plugin # extract zip/tar files
     ];
+  };
+
+  # Enable the settings daemon so Thunar remembers "Show hidden files"
+  programs.xfconf.enable = true;
+
+  # Enable the virtual file system so external drives appear in the sidebar
+  services.gvfs.enable = true;
+
+  # Enable thumbnail support for images and PDFs in Thunar
+  services.tumbler.enable = true;
 }
