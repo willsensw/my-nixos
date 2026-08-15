fc0fc5e9b5a30d891e906ad0df4e4e8f17e70474
Willsen S. Wijaya <willsensw@proton.me>
2026-08-15 16:47:19 +0700

    Update niri.nix

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -7,4 +7,12 @@
 
   # swaylock
   security.pam.services.swaylock = {};
+
+  environment.sessionVariables = {
+    # Force Electron applications (Arduino IDE, Discord, VSCode) to use native Wayland
+    NIXOS_OZONE_WL = "1";
+    
+    # Tell Qt applications (Cura) to try Wayland first, but safely fall back to X11 (xcb) if they panic
+    QT_QPA_PLATFORM = "wayland;xcb";
+  };
 }
