16f3e956ab6544006281b142cd3952af443ee259
Willsen S. Wijaya <willsensw@proton.me>
2026-08-19 21:23:09 +0700

    pywal

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -5,13 +5,13 @@
     niri.enable = true;
     appimage.binfmt = true;
     xwayland.enable = true;
-    pywal.enable = true;
   };
 
   services.power-profiles-daemon.enable = true;
 
   environment.systemPackages = with pkgs; [
     xwayland-satellite
+    pywal
     rofi
     waybar
     ranger
