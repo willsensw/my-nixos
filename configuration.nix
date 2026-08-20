97b8d5b7dfe1b7a0089434f5e9a002f4adc0adfb
Willsen S. Wijaya <willsensw@proton.me>
2026-08-20 08:22:58 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -56,10 +56,8 @@
 
   xdg.portal = {
     enable = true;
-    xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome 
     ];
   };
 
