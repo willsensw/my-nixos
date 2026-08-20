12f680d24441483a5a2fd1ea86576002ded8a2d6
Willsen S. Wijaya <willsensw@proton.me>
2026-08-20 08:13:41 +0700

    Update stm32.nix

--- modules/others/stm32.nix
+++ modules/others/stm32.nix
@@ -1,22 +1,14 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
 	environment.systemPackages = with pkgs; [
-  		# The essential toolchain
   		gcc-arm-embedded  # The compiler for STM32
   		stlink            # For flashing via ST-Link
-  		stm32flash        # Specifically for UART/FT232RL flashing
+		gnumake
   		openocd           # The "Universal" debugger/flasher
-		dfu-util		  # Essential for flashing Black Pill directly via USB
 	];
 
-	# Crucial: This fixes the "Permission Denied" for your FT232RL and STM32
 	services.udev.packages = with pkgs; [ 
 		stlink 
-		openocd
-		dfu-util
 	];
-
-	# How to flash FT232RL
-	# stm32flash -w your_firmware.bin -v -g 0x0 /dev/ttyUSB0
 }
