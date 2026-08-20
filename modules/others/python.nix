5b02217d46566ca82a343d96a6986e7a690fce65
Willsen S. Wijaya <willsensw@proton.me>
2026-08-20 08:11:46 +0700

    Update python.nix

--- modules/others/python.nix
+++ modules/others/python.nix
@@ -4,12 +4,8 @@
 	environment.systemPackages = with pkgs; [
 		python314
 		(python3.withPackages(ps: with ps; [
-    			numpy       # Math for signal processing
-    			matplotlib  # Plotting graphs for reports
-    			pyserial    # UART communication with the FPGA
-    			spidev      # SPI communication (if simulating on Pi)
-    			fastapi     # For building the IoT Web Server later
-    			uvicorn     # Server runner
+    		numpy       # Math for signal processing
+    		matplotlib  # Plotting graphs for reports
   		]))
 	];
 }
